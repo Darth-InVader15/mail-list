09293299f29260b32bce62510f62b0fdc0b83578
D-4 <piyush.singh1315@gmail.com>
2024-01-04 03:26:20 +0530

    added styles to the signup page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path"); 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname)));
 
 
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/signup.html");
+    // app.use(express.static(__dirname + "/signup.css"));
 })
 
 app.post("/",function(req,res){
@@ -15,6 +18,6 @@ app.post("/",function(req,res){
 
 
 
-app.listen("3000",function(){
+app.listen("3001",function(){
     console.log("Its on");
 })
