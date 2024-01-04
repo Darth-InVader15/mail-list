4d1770a5f9037eda2c42dd432e4a9c6ab42809c6
D-4 <piyush.singh1315@gmail.com>
2024-01-05 01:33:42 +0530

    POSTs requests of new contacts using Brevo API

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const path = require("path"); 
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static("public")); //for loading the css and images
+// API Key Placement (using example's approach)
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+defaultClient.authentications['api-key'].apiKey = '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';
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
-    // app.use(express.static(__dirname + "/signup.css"));
-})
+});
 
-app.post("/",function(req,res){
-    const name = req.body.Name;
+app.post("/", function (req, res) {
+    const Fname = req.body.FName;
+    const Lname = req.body.LName;
     const mail = req.body.email;
 
-    console.log(name);
-    // res.write(mail);
-    // res.send();
-})
+    console.log(mail);
 
+    let apiInstance = new SibApiV3Sdk.ContactsApi();
 
+    // Simplified request body (initially)
+    let createContact = {
+        email: mail,
+        listIds: [2]
+    };
 
-app.listen("3001",function(){
-    console.log("Its on");
-})
+    // Conditionally add attributes if needed
+    if (Fname && Lname) {
+        createContact.attributes = {
+            FNAME: Fname,
+            LNAME: Lname
+        };
+    }
+
+    // Logging for debugging
+    // console.log("Request body:", createContact);
+
+    apiInstance.createContact(createContact)
+    .then(function (data) {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        res.sendFile(__dirname + "/fetched.html");
+    })
+    .catch(function (error) {
+        console.error("API error:", error.text);
+        res.sendFile(__dirname + "/failed.html");
+    });
+});
+
+app.listen("3001", function () {
+    console.log("Server is running on port 3001");
+});
+
+// api key
+// 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
