c88ae07eae3970c54b55360e8bc86f802aca04f3
D-4 <piyush.singh1315@gmail.com>
2024-01-05 02:00:03 +0530

    minor bug fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // API Key Placement (using example's approach)
 let defaultClient = SibApiV3Sdk.ApiClient.instance;
-defaultClient.authentications['api-key'].apiKey = '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';
+defaultClient.authentications['api-key'].apiKey = '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';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -20,23 +20,27 @@ app.post("/", function (req, res) {
     const Lname = req.body.LName;
     const mail = req.body.email;
 
-    console.log(mail);
+    // console.log(Fname);
 
     let apiInstance = new SibApiV3Sdk.ContactsApi();
 
     // Simplified request body (initially)
     let createContact = {
         email: mail,
-        listIds: [2]
+        listIds: [3],
+        attributes: {
+            "FIRSTNAME": Fname,
+            "LASTNAME": Lname
+        }
     };
 
     // Conditionally add attributes if needed
-    if (Fname && Lname) {
-        createContact.attributes = {
-            FNAME: Fname,
-            LNAME: Lname
-        };
-    }
+    // if (true) {
+    //     createContact.attributes = {
+    //         "FNAME": Fname,
+    //         "LNAME": Lname
+    //     };
+    // }
 
     // Logging for debugging
     // console.log("Request body:", createContact);
